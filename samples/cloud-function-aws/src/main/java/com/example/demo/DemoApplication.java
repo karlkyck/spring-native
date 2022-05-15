b24430eb35a40e6c2d42582dc78faf4fde040bcd
Karl Kyck <karlkyck@gmail.com>
2022-05-15 14:10:24 +0100

    Added functional style bean declaration for fast startup. Web application type set to "none" to prevent embedded web server from starting

--- samples/cloud-function-aws/src/main/java/com/example/demo/DemoApplication.java
+++ samples/cloud-function-aws/src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,25 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.function.context.FunctionRegistration;
+import org.springframework.cloud.function.context.FunctionalSpringApplication;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.nativex.hint.TypeHint;
 
+@TypeHint(types = GenericApplicationContext.class,
+          typeNames = "org.springframework.context.support.GenericApplicationContext$init")
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements ApplicationContextInitializer<GenericApplicationContext> {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        FunctionalSpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    public void initialize(final GenericApplicationContext context) {
+        context.registerBean("nameUppercaser",
+                             FunctionRegistration.class,
+                             () -> new FunctionRegistration<>(new NameUppercaser()).type(NameUppercaser.class));
+    }
 }
